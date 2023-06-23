f92f475fa424292fc474b2adb9cdef0641e8e24b
vili <me@vili.dev>
2023-06-23 20:35:19 +0300

    Improved ModuleTabs

--- src/main/java/dev/vili/haiku/gui/tabs/ModuleTabs.java
+++ src/main/java/dev/vili/haiku/gui/tabs/ModuleTabs.java
@@ -36,6 +36,7 @@ public class ModuleTabs {
      */
     public static void render() {
         ImGui.getIO().addConfigFlags(ImGuiConfigFlags.NavEnableKeyboard);
+
         for (Module module : Haiku.getInstance().getModuleManager().modules) {
             showSettingsMap.put(module, showSettingsMap.getOrDefault(module, false));
             enabledMap.put(module, new ImBoolean(module.isEnabled()));
@@ -46,87 +47,119 @@ public class ModuleTabs {
 
             for (Setting setting : module.settings) {
                 switch (setting.getClass().getSimpleName()) {
-                    case "BooleanSetting" ->
-                            settingsMap.put(setting, new ImBoolean(((BooleanSetting) setting).isEnabled()));
-                    case "NumberSetting" ->
-                            settingsMap.put(setting, new float[]{(float) ((NumberSetting) setting).getValue()});
+                    case "BooleanSetting" -> settingsMap.put(setting, new ImBoolean(((BooleanSetting) setting).isEnabled()));
+                    case "NumberSetting" -> settingsMap.put(setting, new float[]{(float) ((NumberSetting) setting).getValue()});
                     case "ModeSetting" -> settingsMap.put(setting, new ImInt(((ModeSetting) setting).index));
-                    case "KeybindSetting" ->
-                            settingsMap.put(setting, new ImInt(((KeybindSetting) setting).getKeyCode()));
-                    case "StringSetting" ->
-                            settingsMap.put(setting, new ImString(((StringSetting) setting).getString()));
+                    case "KeybindSetting" -> settingsMap.put(setting, new ImInt(((KeybindSetting) setting).getKeyCode()));
+                    case "StringSetting" -> settingsMap.put(setting, new ImString(((StringSetting) setting).getString()));
                     default -> HaikuLogger.logger.warn("Unknown setting type: " + setting.getClass().getSimpleName());
                 }
             }
-
         }
 
         for (Module.Category category : Module.Category.values()) {
+            if (categoryMap.get(category)) {
+                continue;
+            }
+
             ImGui.begin(category.name(), ImGuiWindowFlags.NoResize);
             if (!categoryMap.get(category)) {
                 ImGui.setWindowSize(250 * HaikuGui.guiWidth.get(), 300 * HaikuGui.guiHeight.get());
                 categoryMap.put(category, true);
             }
 
-            for (Module module : Haiku.getInstance().getModuleManager().getModulesByCategory(category)) {
-                ImGui.checkbox(module.getName(), enabledMap.get(module));
-                if (ImGui.isItemClicked(1)) showSettingsMap.put(module, !showSettingsMap.get(module));
-                ImGui.sameLine(220);
-                if (ImGui.isItemHovered()) ImGui.setTooltip(module.getDescription());
-                ImGui.text(((module.settings.isEmpty()) ? "" : (showSettingsMap.get(module)) ? "^" : "v"));
+            renderCategoryModules(category);
 
-                if (showSettingsMap.get(module)) {
-                    for (Setting setting : module.settings) {
-                        if (module.settings != null) {
-                            switch (setting.getClass().getSimpleName()) {
-                                case "BooleanSetting" -> {
-                                    ImGui.checkbox(setting.name, (ImBoolean) settingsMap.get(setting));
-                                    if (((BooleanSetting) setting).isEnabled() != ((ImBoolean) settingsMap.get(setting)).get())
-                                        ((BooleanSetting) setting).setEnabled(((ImBoolean) settingsMap.get(setting)).get());
-                                }
-                                case "NumberSetting" -> {
-                                    ImGui.sliderFloat(setting.name, (float[]) settingsMap.get(setting), (float) ((NumberSetting) setting).getMinimum(),
-                                            (float) ((NumberSetting) setting).getMaximum());
-                                    float[] temp = (float[]) settingsMap.get(setting);
-                                    if (temp[0] != (float) ((NumberSetting) setting).getValue())
-                                        ((NumberSetting) setting).setValue(temp[0]);
-                                }
-                                case "ModeSetting" -> {
-                                    String[] temp = ((ModeSetting) setting).modes.toArray(new String[0]);
-                                    ImGui.combo(setting.name, (ImInt) settingsMap.get(setting), temp);
-                                    if (((ImInt) settingsMap.get(setting)).get() != ((ModeSetting) setting).modes.indexOf(((ModeSetting) setting).getMode()))
-                                        ((ModeSetting) setting).setMode(((ModeSetting) setting).modes.get(((ImInt) settingsMap.get(setting)).get()));
-                                }
-                                case "KeybindSetting" -> {
-                                    if (binding) {
-                                        ImGui.text("Press a key to bind");
-                                        for (int i = 0; i < 512; i++) {
-                                            if (ImGui.isKeyPressed(i)) {
-                                                if (i == GLFW.GLFW_KEY_ESCAPE || i == GLFW.GLFW_KEY_BACKSPACE || i == GLFW.GLFW_KEY_DELETE) {
-                                                    ((KeybindSetting) setting).setKeyCode(-1);
-                                                } else {
-                                                    ((KeybindSetting) setting).setKeyCode(i);
-                                                }
-                                                binding = false;
-                                            }
-                                        }
+            ImGui.end();
+        }
+    }
+
+    /**
+     * Renders the modules for a given category.
+     *
+     * @param category The category to render modules for
+     */
+    private static void renderCategoryModules(Module.Category category) {
+        for (Module module : Haiku.getInstance().getModuleManager().getModulesByCategory(category)) {
+            ImGui.checkbox(module.getName(), enabledMap.get(module));
+            if (ImGui.isItemClicked(1)) {
+                showSettingsMap.put(module, !showSettingsMap.get(module));
+            }
+            ImGui.sameLine(220);
+            if (ImGui.isItemHovered()) {
+                ImGui.setTooltip(module.getDescription());
+            }
+            ImGui.text(((module.settings.isEmpty()) ? "" : (showSettingsMap.get(module)) ? "^" : "v"));
+
+            if (showSettingsMap.get(module)) {
+                ImGui.indent();
+                renderModuleSettings(module);
+                ImGui.unindent();
+            }
+
+            if (enabledMap.get(module).get() != module.isEnabled()) {
+                module.toggle();
+            }
+        }
+    }
+
+    /**
+     * Renders the settings for a given module.
+     *
+     * @param module The module to render settings for
+     */
+    private static void renderModuleSettings(Module module) {
+        for (Setting setting : module.settings) {
+            if (module.settings != null) {
+                switch (setting.getClass().getSimpleName()) {
+                    case "BooleanSetting" -> {
+                        ImGui.checkbox(setting.name, (ImBoolean) settingsMap.get(setting));
+                        if (((BooleanSetting) setting).isEnabled() != ((ImBoolean) settingsMap.get(setting)).get()) {
+                            ((BooleanSetting) setting).setEnabled(((ImBoolean) settingsMap.get(setting)).get());
+                        }
+                    }
+                    case "NumberSetting" -> {
+                        ImGui.sliderFloat(setting.name, (float[]) settingsMap.get(setting), (float) ((NumberSetting) setting).getMinimum(),
+                                (float) ((NumberSetting) setting).getMaximum());
+                        float[] temp = (float[]) settingsMap.get(setting);
+                        if (temp[0] != (float) ((NumberSetting) setting).getValue()) {
+                            ((NumberSetting) setting).setValue(temp[0]);
+                        }
+                    }
+                    case "ModeSetting" -> {
+                        String[] temp = ((ModeSetting) setting).modes.toArray(new String[0]);
+                        ImGui.combo(setting.name, (ImInt) settingsMap.get(setting), temp);
+                        if (((ImInt) settingsMap.get(setting)).get() != ((ModeSetting) setting).modes.indexOf(((ModeSetting) setting).getMode())) {
+                            ((ModeSetting) setting).setMode(((ModeSetting) setting).modes.get(((ImInt) settingsMap.get(setting)).get()));
+                        }
+                    }
+                    case "KeybindSetting" -> {
+                        if (binding) {
+                            ImGui.text("Press a key to bind");
+                            for (int i = 0; i < 512; i++) {
+                                if (ImGui.isKeyPressed(i)) {
+                                    if (i == GLFW.GLFW_KEY_ESCAPE || i == GLFW.GLFW_KEY_BACKSPACE || i == GLFW.GLFW_KEY_DELETE) {
+                                        ((KeybindSetting) setting).setKeyCode(-1);
                                     } else {
-                                        String name = ((KeybindSetting) setting).getKeyCode() < 0 ? "NONE"
-                                                : InputUtil.fromKeyCode(((KeybindSetting) setting).getKeyCode(), -1).getLocalizedText().getString();
-                                        if (ImGui.button("Bind: " + name)) {
-                                            binding = true;
-                                        }
+                                        ((KeybindSetting) setting).setKeyCode(i);
                                     }
+                                    binding = false;
                                 }
-                                default -> ImGui.text("Unknown setting type");
+                            }
+                        } else {
+                            String name = ((KeybindSetting) setting).getKeyCode() < 0 ? "NONE"
+                                    : InputUtil.fromKeyCode(((KeybindSetting) setting).getKeyCode(), -1).getLocalizedText().getString();
+                            if (ImGui.button("Bind: " + name)) {
+                                binding = true;
                             }
                         }
-                        if (ImGui.isItemHovered()) ImGui.setTooltip(setting.getDescription());
                     }
+                    default -> ImGui.text("Unknown setting type");
                 }
-                if (enabledMap.get(module).get() != module.isEnabled()) module.toggle();
             }
-            ImGui.end();
+            if (ImGui.isItemHovered()) {
+                ImGui.setTooltip(setting.getDescription());
+            }
         }
     }
 }
